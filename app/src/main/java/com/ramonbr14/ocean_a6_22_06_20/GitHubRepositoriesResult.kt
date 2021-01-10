47e89460e7a7fcc1efdd567cc3c4d952b2a9e586
Ramonbr14 <ramonbr14@gmail.com>
2021-01-10 15:45:13 -0400

    Este App Pesquisa repositorios para uma determinada linguagem no github

---
+++ app/src/main/java/com/ramonbr14/ocean_a6_22_06_20/GitHubRepositoriesResult.kt
@@ -0,0 +1,21 @@
+package com.ramonbr14.ocean_a6_22_06_20
+
+data class GitHubRepositoriesResult(
+    val items: List<Repository>
+)
+
+data class Repository(
+    val id: Int,
+    val name: String,
+    val full_name: String,
+    val description: String,
+    val owner: Owner,
+    val html_url: String
+)
+
+data class Owner(
+    val id: Int,
+    val login: String,
+    val avatar_url: String,
+    val html_url: String
+)
